17903baea4725b2fe9f306672d578ad8f376e8b1
Shiva <dhruvashivam@gmail.com>
2024-07-26 10:41:35 +0530

    Fixed js

--- script.js
+++ script.js
@@ -10,18 +10,4 @@ moveRandom.addEventListener("mouseenter", function (e) {
   moveRandomEl(e.target);
 });
 
-function setOutputText() {
-  // Get URL parameters
-  const params = new URLSearchParams(window.location.search);
-
-  // Get the name parameter from the URL
-  const nameParam = params.get('name');
-  const name = nameParam ? 'Do you love me? 😊 ' + nameParam : 'Do you love me? 😊';
-
-  // Find the element with the specific ID and set its text content
-  const outputElement = document.getElementById('output');
-  if (outputElement) {
-    outputElement.textContent = name;
-  }
-}
 
